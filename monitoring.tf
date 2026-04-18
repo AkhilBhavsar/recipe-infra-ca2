1917e6f4a721aec4669e5a5c84e5b212e2ff5c73
AkhilBhavsar <akhilbhavsar09@gmail.com>
2026-04-18 18:58:43 +0100

    feat: add Velero backup storage account and blob container

--- monitoring.tf
+++ monitoring.tf
@@ -5,3 +5,27 @@ resource "azurerm_log_analytics_workspace" "aks" {
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
+
+# Velero backup storage
+resource "azurerm_storage_account" "velero" {
+  name                     = "recipevelero${substr(md5(azurerm_resource_group.main.id), 0, 6)}"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "velero" {
+  name                  = "velero-backups"
+  storage_account_name  = azurerm_storage_account.velero.name
+  container_access_type = "private"
+}
+
+output "velero_storage_account" {
+  value = azurerm_storage_account.velero.name
+}
+
+output "velero_storage_key" {
+  value     = azurerm_storage_account.velero.primary_access_key
+  sensitive = true
+}
